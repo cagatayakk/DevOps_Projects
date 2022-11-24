691823c789d33a154410be5949fc905cc7066be6
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2022-11-24 15:21:27 +0100

    Update sec-gr.tf

--- Project-201-Terraform-Phonebook-Application-deployed-on-AWS/tf-files/sec-gr.tf
+++ Project-201-Terraform-Phonebook-Application-deployed-on-AWS/tf-files/sec-gr.tf
@@ -1,72 +1,78 @@
 resource "aws_security_group" "alb-sg" {
-  name = "ALBSecurityGroup"
-  vpc_id = data.aws_vpc.selected.id
-  tags = {
-    Name = "TF_ALBSecurityGroup"
-  }
+  name        = "ALBSecurityGroup"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = data.aws_vpc.selected.id
 
   ingress {
-    from_port = 80
-    protocol = "tcp"
-    to_port = 80
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ALBSecurityGroup"
   }
 }
 
 resource "aws_security_group" "server-sg" {
-  name = "WebServerSecurityGroup"
-  vpc_id = data.aws_vpc.selected.id
-  tags = {
-    Name = "TF_WebServerSecurityGroup"
+  name        = "WebServerSecurityGroup"
+  description = "Allow HTTP inbound traffic from ALB"
+  vpc_id      = data.aws_vpc.selected.id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 80
-    protocol = "tcp"
-    to_port = 80
-    security_groups = [aws_security_group.alb-sg.id]
-  }
-
-  ingress {
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    security_groups = [ aws_security_group.alb-sg.id ]
   }
 
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "WebServerSecurityGroup"
   }
 }
 
 resource "aws_security_group" "db-sg" {
-  name   = "RDSSecurityGroup"
-  vpc_id = data.aws_vpc.selected.id
-  tags = {
-    "Name" = "TF_RDSSecurityGroup"
-  }
+  name        = "RDSSecurityGroup"
+  description = "Allow RDS inbound traffic from Web Server"
+  vpc_id      = data.aws_vpc.selected.id
 
   ingress {
-    from_port = 3306
-    protocol = "tcp"
-    to_port = 3306
-    security_groups = [aws_security_group.server-sg.id]
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    security_groups = [ aws_security_group.server-sg.id ]
   }
 
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "RDSSecurityGroup"
   }
 }
