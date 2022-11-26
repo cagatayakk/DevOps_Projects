96df48e6aad89fa9ab1466dbfd979b75bc9b0fed
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2022-11-26 23:31:13 +0100

    Update user-data.sh

--- Project-201-Terraform-Phonebook-Application-deployed-on-AWS/user-data.sh
+++ Project-201-Terraform-Phonebook-Application-deployed-on-AWS/user-data.sh
@@ -4,6 +4,6 @@ yum install python3 -y
 pip3 install flask
 pip3 install flask_mysql
 yum install git -y
-TOKEN="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-cd /home/ec2-user && git clone https://$TOKEN@github.com/cagatayakk/DevOps_Projects/tree/main/Project-201-Terraform-Phonebook-Application-deployed-on-AWS
+TOKEN="xxxxxxxxxxxxxxxxxxxxxxx"
+cd /home/ec2-user && git clone https://$TOKEN@github.com/cagatayakk/phonebook.git
 python3 /home/ec2-user/phonebook/phonebook-app.py
