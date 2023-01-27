de1a1f15c5e6e5973c052cf6c027cdb2d5409ce5
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2023-01-27 13:08:53 +0100

    Create variables.tf

---
+++ Project-207-Jenkins-Pipeline-for-WebPage-Application-(Postgresql-Nodejs-React)/jenkins_server/variables.tf
@@ -0,0 +1,19 @@
+variable "mykey" {
+  default = "cagatayakk"
+}
+variable "myami" {
+  default = "ami-026b57f3c383c2eec"
+}
+variable "instancetype" {
+  default = "t3a.medium"
+}
+variable "tag" {
+  default = "Jenkins_Server"
+}
+variable "jenkins-sg" {
+  default = "jenkins-server-sec-gr-207"
+}
+
+variable "user" {
+  default = "cagatay"
+}
