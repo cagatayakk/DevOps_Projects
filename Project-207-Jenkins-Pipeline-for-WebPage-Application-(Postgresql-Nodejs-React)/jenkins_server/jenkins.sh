e26a1c7da7c80b76352c78ba61864fea13313ac4
M. Cagatay Akkiran <109696768+cagatayakk@users.noreply.github.com>
2023-12-28 23:22:49 +0100

    Update jenkins.sh

--- Project-207-Jenkins-Pipeline-for-WebPage-Application-(Postgresql-Nodejs-React)/jenkins_server/jenkins.sh
+++ Project-207-Jenkins-Pipeline-for-WebPage-Application-(Postgresql-Nodejs-React)/jenkins_server/jenkins.sh
@@ -2,8 +2,8 @@
 yum update -y
 hostnamectl set-hostname jenkins-server
 yum install git -y
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum upgrade
 amazon-linux-extras install java-openjdk11 -y
 yum install jenkins -y
